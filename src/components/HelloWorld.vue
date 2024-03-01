b9370914ed8770de7085f838c553793a82f435c7
itsiky <itsiky@gmail.com>
2024-03-01 05:13:24 +0200

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,7 +32,7 @@ const count = ref(0)
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Made by itsik yehuda</p> 
+  <p class="read-the-docs">Made by itsik yehuda.</p>
 </template>
 
 <style scoped>
